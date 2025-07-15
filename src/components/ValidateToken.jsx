c0694667ba1f187d1f2465d9c716f01955d8c110
Adrian Tan E-Herng <adriantan3921@gmail.com>
2025-07-15 15:20:16 +0800

    Minor bug fix on validation

--- src/components/ValidateToken.jsx
+++ src/components/ValidateToken.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export async function validateToken(token) {
     try {
-        const res = await axios.post("https://d6f2da03-86f1-44a0-b30a-17b0e56123a4-00-jaox2jqn66ea.sisko.replit.dev/validate-token", { token: token });
+        const res = await axios.post("https://gym-api-mauve.vercel.app", { token: token });
         console.log("The token is validated")
         return res.data.valid === true;
     } catch (err) {
